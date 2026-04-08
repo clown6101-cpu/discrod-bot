11b5a365624e154b2b48dc61535ee2d9fb2034c1
clown6101-cpu <clown6101@gmail.com>
2026-04-08 16:56:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent] });
 
 client.once('clientReady', () => {
   console.log(`Logged in as ${client.user.tag}`);
